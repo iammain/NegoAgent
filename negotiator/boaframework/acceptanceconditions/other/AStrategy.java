332593c753bcc2682272ebe5a64df29c13707da8
iammain <k1rill@inbox.ru>
2013-11-14 16:09:01 +0100

    changes.

---
+++ negotiator/boaframework/acceptanceconditions/other/AStrategy.java
@@ -0,0 +1,67 @@
+package negotiator.boaframework.acceptanceconditions.other;
+
+import java.util.HashMap;
+import negotiator.boaframework.AcceptanceStrategy;
+import negotiator.boaframework.Actions;
+import negotiator.boaframework.NegotiationSession;
+import negotiator.boaframework.OfferingStrategy;
+import negotiator.boaframework.OpponentModel;
+
+/**
+ * This Acceptance Condition will accept an opponent bid if the utility is higher than the 
+ * bid the agent is ready to present
+ * 
+ * Decoupling Negotiating Agents to Explore the Space of Negotiation Strategies
+ * T. Baarslag, K. Hindriks, M. Hendrikx, A. Dirkzwager, C.M. Jonker
+ * 
+ * @author Alex Dirkzwager, Mark Hendrikx
+ * @version 18/12/11
+ */
+public class AStrategy extends AcceptanceStrategy {
+	
+	private double a;
+	private double b;
+
+	/**
+	 * Empty constructor for the BOA framework.
+	 */
+	public AStrategy() { }
+	
+	public AStrategy(NegotiationSession negoSession, OfferingStrategy strat, double alpha, double beta){
+		this.negotiationSession = negoSession;
+		this.offeringStrategy = strat;
+		this.a =  alpha;
+		this.b = beta;
+	}
+
+	@Override
+	public void init(NegotiationSession negoSession, OfferingStrategy strat, OpponentModel opponentModel, HashMap<String, Double> parameters) throws Exception {
+		this.negotiationSession = negoSession;
+		this.offeringStrategy = strat;
+
+		if (parameters.get("a") != null || parameters.get("b") != null) {
+			a = parameters.get("a");
+			b = parameters.get("b");
+		} else {
+			a = 1;
+			b = 0;
+		}
+	}
+	
+	@Override
+	public String printParameters() {
+		String str = "[a: " + a + " b: " + b + "]";
+		return str;
+	}
+
+	@Override
+	public Actions determineAcceptability() {
+		double nextMyBidUtil = offeringStrategy.getNextBid().getMyUndiscountedUtil();
+		double lastOpponentBidUtil = negotiationSession.getOpponentBidHistory().getLastBidDetails().getMyUndiscountedUtil();
+		
+		if (a * lastOpponentBidUtil + b >= nextMyBidUtil) {
+			return Actions.Accept;
+		}
+		return Actions.Reject;
+	}
+}
