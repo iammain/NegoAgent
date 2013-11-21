b29d77f9cbdea1d264ad31190a4dff5a351d53d1
Slave <p.hadjichristodoulou@hotmail.com>
2013-11-21 11:08:05 +0100

    AStrategy Updated
    Recompile , put AS strategy to
    negotiator/boaframework/acceptanceconditions/other
    .jar

--- negotiator/boaframework/acceptanceconditions/other/AStrategy.java
+++ negotiator/boaframework/acceptanceconditions/other/AStrategy.java
@@ -1,11 +1,16 @@
 package negotiator.boaframework.acceptanceconditions.other;
 
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import negotiator.Bid;
+import negotiator.BidHistory;
 import negotiator.boaframework.AcceptanceStrategy;
 import negotiator.boaframework.Actions;
 import negotiator.boaframework.NegotiationSession;
 import negotiator.boaframework.OfferingStrategy;
 import negotiator.boaframework.OpponentModel;
+import negotiator.utility.UtilitySpace;
 
 /**
  * This Acceptance Condition will accept an opponent bid if the utility is higher than the 
@@ -18,9 +23,13 @@ import negotiator.boaframework.OpponentModel;
  * @version 18/12/11
  */
 public class AStrategy extends AcceptanceStrategy {
-	
+    
+        private double ACCEPTANCE_THRESHOLD =  Integer.MIN_VALUE;
+        private double lamda;
 	private double a;
 	private double b;
+        private double c;
+        private double preassureThreshold;
 
 	/**
 	 * Empty constructor for the BOA framework.
@@ -32,6 +41,7 @@ public class AStrategy extends AcceptanceStrategy {
 		this.offeringStrategy = strat;
 		this.a =  alpha;
 		this.b = beta;
+                this.c = .3;
 	}
 
 	@Override
@@ -43,8 +53,11 @@ public class AStrategy extends AcceptanceStrategy {
 			a = parameters.get("a");
 			b = parameters.get("b");
 		} else {
-			a = 1;
-			b = 0;
+			a = .7;
+			b = .2;
+                        c = .2;
+                        lamda =.1 + .9* Math.pow(negotiationSession.getDiscountFactor(), b);
+                        preassureThreshold = .1;
 		}
 	}
 	@Override
@@ -52,15 +65,51 @@ public class AStrategy extends AcceptanceStrategy {
 		String str = "[a: " + a + " b: " + b + "]";
 		return str;
 	}
-
+        
 	@Override
 	public Actions determineAcceptability() {
-		double nextMyBidUtil = offeringStrategy.getNextBid().getMyUndiscountedUtil();
-		double lastOpponentBidUtil = negotiationSession.getOpponentBidHistory().getLastBidDetails().getMyUndiscountedUtil();
-		
-		if (a * lastOpponentBidUtil + b >= nextMyBidUtil) {
-			return Actions.Accept;
-		}
-		return Actions.Reject;
+            System.out.println("New Debug");
+            
+            try {
+                double negTime    = negotiationSession.getTime();
+                double prediction =negotiationSession.getOpponentBidHistory().filterBetweenTime(negTime-.001,  negTime).size();
+                if(negotiationSession.getTime()!=0){
+                    if(prediction!=1){
+                        c = 1/prediction;
+                        lamda = lamda+(1-lamda)*Math.pow(prediction, c );
+                        lamda = lamda*negTime*2;
+                    }
+                }
+                
+                
+                UtilitySpace bH = negotiationSession.getUtilitySpace();                
+                if(negotiationSession.getTime()>lamda){
+                    double maxUtility = bH.getUtility(bH.getMaxUtilityBid());
+                    if(ACCEPTANCE_THRESHOLD== Integer.MIN_VALUE)
+                        ACCEPTANCE_THRESHOLD = maxUtility*negotiationSession.getDiscountFactor();
+                    else
+                        ACCEPTANCE_THRESHOLD = ACCEPTANCE_THRESHOLD*negotiationSession.getDiscountFactor();
+                }else{
+                    double maxUtility = bH.getUtility(bH.getMaxUtilityBid());                    
+                    double tempAcceptance = maxUtility-(maxUtility-maxUtility*negotiationSession.getDiscountFactor())*
+                            Math.pow(negotiationSession.getTime()/lamda ,a);
+                    ACCEPTANCE_THRESHOLD = tempAcceptance;
+                }
+                System.out.println("Acceptance threshold " +ACCEPTANCE_THRESHOLD);
+                
+            } catch (Exception ex) {
+                Logger.getLogger(AStrategy.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            double lastOpponentBidUtil = negotiationSession.getOpponentBidHistory().getLastBidDetails().getMyUndiscountedUtil();
+            double nextMyBidUtil = offeringStrategy.getNextBid().getMyUndiscountedUtil();
+            System.out.println("Last oponents bid util " +lastOpponentBidUtil);
+            System.out.println("Next bid util  " + nextMyBidUtil);
+            System.out.println("Current time " + negotiationSession.getTime());
+            double nextThres  = nextMyBidUtil + nextMyBidUtil*preassureThreshold; 
+            System.out.println("Next Threshold " + nextThres);
+            if(lastOpponentBidUtil > ACCEPTANCE_THRESHOLD ||lastOpponentBidUtil > nextThres)
+                    return Actions.Accept;
+            return Actions.Reject;
 	}
 }
