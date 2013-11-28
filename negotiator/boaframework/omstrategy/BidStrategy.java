42b52c7c488ccd81fac4cb77cd0a2ee0b4874a74
Slave <p.hadjichristodoulou@hotmail.com>
2013-11-28 12:23:38 +0100

    b
    b

--- negotiator/boaframework/omstrategy/BidStrategy.java
+++ negotiator/boaframework/omstrategy/BidStrategy.java
@@ -1,107 +1,95 @@
 package negotiator.boaframework.omstrategy;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 import negotiator.bidding.BidDetails;
 import negotiator.boaframework.NegotiationSession;
 import negotiator.boaframework.OMStrategy;
 import negotiator.boaframework.OpponentModel;
 
-/**
- * This class uses an opponent model to determine the next bid for the opponent, while taking
- * the opponent's preferences into account. The opponent model is used to select the best bid.
- *
- * @author Mark Hendrikx
- */
-public class BidStrategy extends OMStrategy {
- 
-	/**  when to stop updating the opponentmodel. Note that this value
-	 * 	 is not exactly one as a match sometimes lasts slightly longer. */
-	double updateThreshold = 1.1;
-	
-	/**
-	 * Empty constructor for the BOA framework.
-	 */
-	public BidStrategy() { }
-
-	/**
-	 * Normal constructor used to initialize the BestBid opponent model strategy.
-	 * @param negotiationSession symbolizing the negotiation state.
-	 * @param model used by the opponent model strategy.
-	 */
-	public BidStrategy(NegotiationSession negotiationSession, OpponentModel model) {
-		try {
-			super.init(negotiationSession, model);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Initializes the opponent model strategy. If a value for the parameter t is given, then
-	 * it is set to this value. Otherwise, the default value is used.
-	 * 
-	 * @param negotiationSession state of the negotiation.
-	 * @param model opponent model used in conjunction with this opponent modeling strategy.
-	 * @param parameters set of parameters for this opponent model strategy.
-	 */
-	public void init(NegotiationSession negotiationSession, OpponentModel model, HashMap<String, Double> parameters) throws Exception {
-		super.init(negotiationSession, model);
-		if (parameters.get("t") != null) {
-			updateThreshold = parameters.get("t").doubleValue();
-		} else {
-			System.out.println("OMStrategy assumed t = 1.1");
-		}
-	}
-	
-	/**
-	 * Returns the best bid for the opponent given a set of similarly
-	 * preferred bids.
-	 * 
-	 * @param list of the bids considered for offering.
-	 * @return bid to be offered to opponent.
-	 */
-	@Override
-	public BidDetails getBid(List<BidDetails> allBids) {
-            BidDetails bestBid = allBids.get(0);
-		
-		// 1. If there is only a single bid, return this bid
-		if (allBids.size() == 1) {
-			return allBids.get(0);
-		}
-		double bestUtil = -1;
-		// 2. Check that not all bids are assigned at utility of 0
-		// to ensure that the opponent model works. If the opponent model
-		// does not work, offer a random bid.
-		boolean allWereZero = true;
-	 	// 3. Determine the best bid
-		for (BidDetails bid : allBids) {
-			double evaluation = model.getBidEvaluation(bid.getBid());
-			if (evaluation > 0.0001) {
-				allWereZero = false;
-			}
-			if (evaluation > bestUtil) {
-				bestBid = bid;
-				bestUtil = evaluation;
-			}
-		}
-		// 4. The opponent model did not work, therefore, offer a random bid.
-		if (allWereZero) {
-			Random r = new Random();
-			return allBids.get(r.nextInt(allBids.size()));
-		}
-		return bestBid;
-	}
-
-	/**
-	 * The opponent model may be updated, unless the time is higher
-	 * than a given constant.
-	 * 
-	 * @return true if model may be updated.
-	 */
-	@Override
-	public boolean canUpdateOM() {
-		return negotiationSession.getTime() < updateThreshold;
-	}
+public final class BidStrategy extends OMStrategy{
+  private ArrayDeque<Double> lastTen;
+  private int secondBestCounter = 1;
+  
+  public BidStrategy() {}
+  
+  public BidStrategy(NegotiationSession negotiationSession, OpponentModel model){
+    initializeAgent(negotiationSession, model);
+  }
+  
+  public void init(NegotiationSession nSession, OpponentModel oppModel, HashMap<String, Double> parameters) throws Exception{
+    initializeAgent(nSession, oppModel);
+  }
+  
+  private void initializeAgent(NegotiationSession negotiationSession, OpponentModel model)
+  {
+    try
+    {
+      super.init(negotiationSession, model);
+      this.lastTen = new ArrayDeque(11);
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+  
+  public BidDetails getBid(List<BidDetails> list){
+    Collections.sort(list, new Comparing(this.model));
+    BidDetails opponentBestBid = (BidDetails)list.get(0);
+    boolean allEqual = true;
+    for (Iterator localIterator = this.lastTen.iterator(); localIterator.hasNext();){
+      double bid = ((Double)localIterator.next()).doubleValue();
+      if (bid != opponentBestBid.getMyUndiscountedUtil()) {
+        allEqual = false;
+      }
+    }
+    if (allEqual)
+    {
+      this.secondBestCounter += 1;
+      if (list.size() > 1) {
+        opponentBestBid = (BidDetails)list.get(1);
+      }
+    }
+    this.lastTen.addLast(Double.valueOf(opponentBestBid.getMyUndiscountedUtil()));
+    if (this.lastTen.size() > 10) {
+      this.lastTen.removeFirst();
+    }
+    return opponentBestBid;
+  }
+  
+  public int getSecondBestCount(){
+    return this.secondBestCounter;
+  }  
+  public boolean canUpdateOM(){
+    return true;
+  }  
+  private static final class Comparing
+    implements Comparator<BidDetails>
+  {
+    private final OpponentModel model;
+    
+    protected Comparing(OpponentModel model)
+    {
+      this.model = model;
+    }
+    
+    @Override
+    public int compare(BidDetails a, BidDetails b)
+    {
+      double evalA = this.model.getBidEvaluation(a.getBid());
+      double evalB = this.model.getBidEvaluation(b.getBid());
+      if (evalA < evalB) {
+        return 1;
+      }
+      if (evalA > evalB) {
+        return -1;
+      }
+      return 0;
+    }
+  }
 }
